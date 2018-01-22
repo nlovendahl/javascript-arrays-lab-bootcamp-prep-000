408899dd4d1f2c15ada029a8b46478c7951d8202
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-01-22 19:05:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function destructivelyRemoveFirstKitten(name) {
 }
 
 function appendKitten(name) {
-  moreKittens = kittens.push(name);
+  var moreKittens = kittens.push(name);
   return moreKittens;
 }
 
